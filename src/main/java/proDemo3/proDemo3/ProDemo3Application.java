aa5ccfbfe706d9ebe846bf618a019d5e98d89c21
aman <amansainigithub>
2023-09-09 09:26:42 +0530

    First Commit

---
+++ src/main/java/proDemo3/proDemo3/ProDemo3Application.java
@@ -0,0 +1,13 @@
+package proDemo3.proDemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProDemo3Application.class, args);
+	}
+
+}
