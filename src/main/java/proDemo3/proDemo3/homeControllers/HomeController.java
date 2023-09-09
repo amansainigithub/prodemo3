ec1ac12b80b3ffdef27ec60b1f2f1d5c6760d6e3
aman <amansainigithub>
2023-09-09 09:46:16 +0530

    First Commit

--- src/main/java/proDemo3/proDemo3/homeControllers/HomeController.java
+++ src/main/java/proDemo3/proDemo3/homeControllers/HomeController.java
@@ -2,6 +2,7 @@ package proDemo3.proDemo3.homeControllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 
-    @GetMapping("/msg")
-    public String getMessage()
+    @GetMapping("/msg/{name}")
+    public String getMessage(@RequestParam String name)
     {
-        return "Works Noe";
+        return "Works Now :::::: " + name;
     }
 
 }
